12512b9ada1c4ab15a35d8d1deb09137caa3db4b
Adham Zaky <Zaky@aucegypt.edu>
2020-04-22 17:03:20 +0200

    a7la messa

---
+++ headers/ZakyAdham.h
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+void ZakyAdham()
+{
+	cout<< "ZakyAdham\n";
+	cout<<"Zaky@aucegypt.edu\n";
+	return;
+}
---
+++ sources/ZakyAdham.cpp
@@ -0,0 +1,5 @@
+include "ZakyAdham.h"
+int main()
+{
+	ZakyAdham();
+}
